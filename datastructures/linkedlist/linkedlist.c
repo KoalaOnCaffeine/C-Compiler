622862cdece8c1822eb920ac792ee7b81ce89efd
Tom <tomtomnewton2005@gmail.com>
2023-07-20 01:23:09 +0100

    Create linked list to use in map buckets

---
+++ datastructures/linkedlist/linkedlist.c
@@ -0,0 +1,121 @@
+//
+// Created by Tom on 13/7/23.
+//
+
+#include <malloc.h>
+#include <stddef.h>
+#include "linkedlist.h"
+
+LinkedList *create_linked_list() {
+    LinkedList *linkedList = malloc(sizeof(LinkedList));
+    linkedList->head = NULL;
+    linkedList->length = 0;
+
+    return linkedList;
+}
+
+void delete_linked_list(LinkedList *linkedList) {
+    Node *next = linkedList->head;
+    while (next != NULL) {
+        Node *current = next;
+        next = next->next;
+        free(current);
+    }
+    free(linkedList);
+}
+
+Node *create_node(void *value) {
+    Node *node = malloc(sizeof(Node));
+    node->value = value;
+    node->next = NULL;
+    return node;
+}
+
+void delete_node(Node *node) {
+    free(node);
+}
+
+void append(LinkedList *linkedList, void *value) {
+    Node *node = create_node(value);
+    if (linkedList->head == NULL) {
+        linkedList->head = node;
+    } else {
+        Node *current = linkedList->head;
+        while (current->next != NULL) current = current->next;
+        current->next = node;
+    }
+    ++linkedList->length;
+}
+
+void prepend(LinkedList *linkedList, void *value) {
+    Node *node = create_node(value);
+    Node *head = linkedList->head;
+    linkedList->head = node;
+    node->next = head;
+    ++linkedList->length;
+}
+
+void insert(LinkedList *linkedList, void *value, unsigned int index) {
+    if (index == 0) return prepend(linkedList, value);
+    Node *previous = linkedList->head;
+    for (unsigned int i = 0; i < index - 1; ++i) {
+        if (previous == NULL) return;
+        previous = previous->next;
+    }
+    ++linkedList->length;
+}
+
+void set_index(LinkedList *linkedList, void *value, unsigned int index) {
+    Node *current = linkedList->head;
+    for (unsigned int i = 0; i < index; ++i) {
+        if (current == NULL) return;
+        current = current->next;
+    }
+    current->value = value;
+}
+
+int remove(LinkedList *linkedList, void *value) {
+    if (linkedList->length == 0) return 0;
+    unsigned int removed = 0;
+
+    Node *prev = linkedList->head; // Check the head last
+
+    while (prev->next != NULL) {
+        if (prev->next->value == value) {
+            Node *current = prev->next;
+            prev->next = current->next;
+            delete_node(current);
+            ++removed;
+        }
+    }
+
+    if (linkedList->head->value == value) {
+        Node *head = linkedList->head;
+        linkedList->head = head->next;
+        delete_node(head);
+        ++removed;
+    }
+
+    linkedList->length -= removed;
+
+    return removed != 0;
+}
+
+int remove_at_index(LinkedList *linkedList, unsigned int index) {
+    if (linkedList->length == 0 || index >= linkedList->length) return 0; // Nothing to remove
+    if (index == 0) {
+        Node *head = linkedList->head;
+        linkedList->head = head->next;
+        delete_node(head);
+    } else {
+        Node *prev = linkedList->head;
+        for (unsigned int i = 0; i < index - 1; ++i) {
+            prev = prev->next;
+        }
+        Node *next = prev->next->next;
+        delete_node(prev->next);
+        prev->next = next;
+    }
+    --linkedList->length;
+    return 1; // Node was removed
+}
---
+++ datastructures/linkedlist/linkedlist.h
@@ -0,0 +1,41 @@
+//
+// Created by Tom on 13/7/23.
+//
+
+#ifndef C_COMPILER_LINKEDLIST_H
+#define C_COMPILER_LINKEDLIST_H
+
+typedef struct Node Node;
+typedef struct LinkedList LinkedList;
+
+struct Node {
+    void *value;
+    Node *next;
+};
+
+struct LinkedList {
+    Node *head;
+    unsigned int length;
+};
+
+LinkedList *create_linked_list();
+
+void delete_linked_list(LinkedList *linkedList);
+
+Node *create_node(void *value);
+
+void delete_node(Node *node);
+
+void append(LinkedList *linkedList, void *value);
+
+void prepend(LinkedList *linkedList, void *value);
+
+void insert(LinkedList *linkedList, void *value, unsigned int index);
+
+void set_index(LinkedList *linkedList, void *value, unsigned int index);
+
+int remove(LinkedList *linkedList, void *value); // Returns whether it was removed
+
+int remove_at_index(LinkedList *linkedList, unsigned int index); // Returns whether it was removed
+
+#endif //C_COMPILER_LINKEDLIST_H
