6befad1719de0a851ec185c32dd870f276371381
Tom <tomtomnewton2005@gmail.com>
2023-08-10 15:38:37 +0100

    Rework the map to support resizing and rehashing, subsequently remove the entry file as it is no longer necessary

--- datastructures/map/map.c
+++ datastructures/map/map.c
@@ -1,85 +1,157 @@
 //
-// Created by Tom on 12/7/23.
+// Created by Tom on 29/7/23.
 //
 
 #include <malloc.h>
-#include <stddef.h>
 #include "map.h"
 
-Map *create_map(int(*equals)(void *, void *), long(*hash)(void *)) {
-    Map *map = malloc(sizeof(Map));
-    map->capacity = DEFAULT_MAP_CAPACITY;
-    map->buckets = calloc(map->capacity, sizeof(LinkedList *));
-    map->equals = equals;
-    map->hash = hash;
+Map *create_map(equals_function equalsFunction, hash_function hashFunction) {
+    Map *map = calloc(1, sizeof(Map));
+    /*
+     * Initialize capacity and entry_count to 0
+     * Initialize buckets to null
+     */
+    map->equalsFunction = equalsFunction;
+    map->hashFunction = hashFunction;
     return map;
 }
 
 void delete_map(Map *map) {
-    if (map == NULL) return;
-    for (unsigned int i = 0, j = map->capacity; i < j; ++i) {
-        delete_linked_list(map->buckets[i]);
-    }
-    free(map->buckets);
+    if (!map) return;
+    for (unsigned int i = 0; i < map->capacity; ++i)
+        delete_bucket(map->buckets[i]);
     free(map);
 }
 
+Bucket *create_bucket() {
+    Bucket *bucket = calloc(1, sizeof(Bucket));
+    return bucket;
+}
+
+void delete_bucket(Bucket *bucket) {
+    Entry *cur = bucket->head;
+    while (cur) {
+        Entry *tmp = cur;
+        cur = cur->next;
+        delete_entry(tmp);
+    }
+    free(bucket);
+}
+
+Entry *create_entry(void *key, void *value) {
+    Entry *entry = calloc(1, sizeof(Entry)); // Initialize prev and next pointers to null
+    entry->key = key;
+    entry->value = value;
+    return entry;
+}
+
+void delete_entry(Entry *entry) {
+    free(entry);
+}
+
 void put(Map *map, void *key, void *value) {
-    long hash = map->hash(key);
-    unsigned long index = hash % map->capacity;
+    // Resize before adding
+    if (4 * map->entry_count >= 3 * map->capacity)
+        resize_map(map, map->capacity ? map->capacity * 2 : 16); // Double, or initially 16
 
-    if (map->buckets[index] == NULL) map->buckets[index] = create_linked_list();
+    long hash = map->hashFunction(key);
+    unsigned int bucket_index = hash % map->capacity; // Capacity will always be larger than 0
+    Bucket *bucket = map->buckets[bucket_index];
 
-    LinkedList *bucket = map->buckets[index];
-    Entry *entry = create_entry(key, value);
-    append(bucket, entry);
+    if (!bucket) map->buckets[bucket_index] = bucket = create_bucket();
+
+    Entry *entry = find_entry(bucket, map->equalsFunction, key);
+    if (entry)
+        return (void) (entry->value = value); // Overriding an existing entry, entry count does not increase
+
+    // Create a new entry
+    entry = create_entry(key, value);
+    ++map->entry_count;
+    add_entry(bucket, entry);
 }
 
-int get(Map *map, void *key, void **value) {
-    long hash = map->hash(key);
-    unsigned long index = hash % map->capacity;
-    LinkedList *bucket = map->buckets[index];
+Entry *remove_key(Map *map, void *key) {
+    if (!map->capacity) return 0;
+    long hash = map->hashFunction(key);
+    unsigned int bucket_index = hash % map->capacity;
+    Bucket *bucket = map->buckets[bucket_index];
+    if (!bucket || !bucket->head) return 0;
 
-    Node *current = bucket ? bucket->head : NULL;
+    Entry *entry = bucket->head;
 
-    for (; current; current = current->next) {
-        if (map->equals(((Entry *) current->value)->key, key)) {
-            *value = (void *) ((Entry *) current->value)->value;
-            return MAP_RETURN_VALUE;
+    while (entry) {
+        if (map->equalsFunction(entry->key, key)) {
+            if (entry == bucket->head) bucket->head = entry->next;
+            else entry->prev->next = entry->next;
+            if (entry == bucket->tail) bucket->tail = entry->prev;
+            else entry->next->prev = entry->prev;
+            --map->entry_count;
+
+            // Note that this is equivalent to if(entry count < capacity/4), meaning that less than a quarter is filled
+            // Don't bother resizing below 16?
+            if (4 * map->entry_count < map->capacity && map->capacity > 16)
+                resize_map(map, map->capacity / 2);
+            return entry;
         }
+        entry = entry->next;
     }
-    return MAP_RETURN_NULL;
+
+    return 0;
 }
 
-void remove_key(Map *map, void *key) {
-    long hash = map->hash(key);
-    unsigned long index = hash % map->capacity;
-    LinkedList *bucket = map->buckets[index];
+Entry *find_entry(Bucket *bucket, equals_function equalsFunction, void *key) {
+    Entry *cur = bucket->head;
+    while (cur) {
+        if (equalsFunction(cur->key, key)) return cur;
+        else cur = cur->next;
+    }
+    return 0;
+}
 
-    if (bucket == NULL) return;
+int get(Map *map, void *key, void **value_ptr) {
+    if (!map->capacity) return 0; // Failure
+    long hash = map->hashFunction(key);
+    unsigned int bucketIndex = hash % map->capacity;
+    Bucket *bucket = map->buckets[bucketIndex];
+    if (!bucket) return 0; // Failure
+    Entry *entry = find_entry(bucket, map->equalsFunction, key);
+    if (!entry) return 0; // Failure
+    *value_ptr = entry->value;
+    return 1; // Success
+}
 
-    // No need to check bucket length, since if the bucket is not null, there must be an entry
-    Node *prev = bucket->head;
+void resize_map(Map *map, unsigned int new_capacity) {
+    map->buckets = realloc(map->buckets, new_capacity * sizeof(Bucket *));
+    for (unsigned int i = map->capacity; i < new_capacity; i++)
+        map->buckets[i] = 0; // Set new bucket pointers to null
+    map->capacity = new_capacity;
+    rehash(map);
+}
 
-    // Handle the head separately
-    if (map->equals(((Entry *) prev->value)->key, key)) {
-        remove_at_index(bucket, 0);
-    } else {
-        while (prev->next != NULL) {
-            if (map->equals(((Entry *) prev->next->value)->key, key)) {
-                Node *current = prev->next;
-                prev->next = current->next;
-                delete_node(current);
-                --bucket->length;
-                break; // If a pair is found, it is guaranteed to be the only possible pair with the key
-            }
+void rehash(Map *map) {
+    Bucket **new_buckets = calloc(map->capacity, sizeof(Bucket *));
+    for (unsigned int i = 0; i < map->capacity; i++) {
+        Bucket *old_bucket = map->buckets[i];
+        if (!old_bucket) continue;
+        Entry *entry = old_bucket->head;
+        while (entry) {
+            long hash = map->hashFunction(entry->key);
+            unsigned int new_bucket_index = hash % map->capacity;
+            Bucket *new_bucket = new_buckets[new_bucket_index];
+            if (!new_bucket) new_buckets[new_bucket_index] = new_bucket = create_bucket();
+            add_entry(new_bucket, entry);
+            entry = entry->next;
         }
     }
+    free(map->buckets); // Free the old memory, but do not use delete_bucket, as this will delete the entries too
+    map->buckets = new_buckets;
+}
 
-    // Delete the bucket, remove the pointer in the list of buckets
-    if (!bucket->length) {
-        map->buckets[index] = NULL;
-        delete_linked_list(bucket);
-    }
+void *add_entry(Bucket *bucket, Entry *entry) {
+    if (bucket->head) {
+        bucket->tail->next = entry;
+        entry->prev = bucket->tail;
+        bucket->tail = entry;
+    } else bucket->head = bucket->tail = entry; // If there is no head, there will be no tail
 
 }
--- datastructures/map/map.h
+++ datastructures/map/map.h
@@ -1,45 +1,77 @@
 //
-// Created by Tom on 12/7/23.
+// Created by Tom on 29/7/23.
 //
-
-#include "entry.h"
-#include "../linkedlist/linkedlist.h"
-
-#ifndef DEFAULT_MAP_CAPACITY
-#define DEFAULT_MAP_CAPACITY 16
-#endif
-#ifndef MAP_RETURN_NULL
-#define MAP_RETURN_NULL 0
-#endif
-#ifndef MAP_RETURN_VALUE
-#define MAP_RETURN_VALUE 1
-#endif
 
 #ifndef C_COMPILER_MAP_H
 #define C_COMPILER_MAP_H
 
-/*
- * The map is a list of entries
- * Buckets store multiple entries which all have colliding hashes
- */
+typedef struct Map Map;
+typedef struct Bucket Bucket;
+typedef struct Entry Entry;
 
-typedef struct {
-    unsigned int capacity; // The number of buckets in the map
-    LinkedList **buckets; // The array of bucket pointers
+typedef int (*equals_function)(void *, void *);
 
-    int (*equals)(void *, void *);
+typedef long (*hash_function)(void *);
 
-    long (*hash)(void *); // A hashing function
-} Map;
+struct Map {
+    unsigned int capacity; // The number of bucket
+    unsigned int entry_count; // The number of entries
+    Bucket **buckets; // Array of buckets
+    equals_function equalsFunction;
+    hash_function hashFunction;
+};
 
-Map *create_map(int(*equals)(void *, void *), long(*hash)(void *));
+struct Bucket {
+    Entry *head; // The first entry in the bucket
+    Entry *tail; // The last entry in the bucket - used for faster additions
+};
 
+struct Entry {
+    void *key; // The key associated with the entry
+    void *value; // The value associated with the entry
+    Entry *prev; // The previous entry
+    Entry *next; // The next entry
+};
+
+// Memory functions
+
+Map *create_map(equals_function equalsFunction, hash_function hashFunction);
+
+// Deletes a map and all associated memory
 void delete_map(Map *map);
 
+// Creates an empty bucket with no head or tail
+Bucket *create_bucket();
+
+// Deletes a bucket and all associated memory
+void delete_bucket(Bucket *bucket);
+
+// Creates an empty entry with a null pointer key, value and previous and next entries
+Entry *create_entry(void *key, void *value);
+
+void delete_entry(Entry *entry);
+
+// Map functions
+
+// Associates the key to a value
 void put(Map *map, void *key, void *value);
 
-int get(Map *map, void *key, void **value);
+// Gets the associated value from a given key, and returns whether a value was found
+int get(Map *map, void *key, void **value_ptr);
 
-void remove_key(Map *map, void *key);
+// Disassociates a value from a key, returning the removed entry
+Entry *remove_key(Map *map, void *key);
+
+// Finds an entry in a bucket, given a key. Returns the entry
+Entry *find_entry(Bucket *bucket, equals_function equalsFunction, void *key); // Helper function
+
+// Add an entry to a bucket
+void *add_entry(Bucket *bucket, Entry *entry);
+
+// Change the size of the map (note that this should call rehash immediately after resizing)
+void resize_map(Map *map, unsigned int new_capacity);
+
+// Recalculate the buckets for each entry in the map
+void rehash(Map *map);
 
 #endif //C_COMPILER_MAP_H
